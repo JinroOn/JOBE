b73cd4729bad9143b85079137452dd340bcbfd02
vkfro2293 <vkfro2293@naver.com>
2026-04-27 00:32:11 +0900

    feat: 스프링 부트 프로젝트 init

---
+++ src/main/java/com/jinroon/jobe/JobeApplication.java
@@ -0,0 +1,13 @@
+package com.jinroon.jobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jinroon/jobe/JobeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinroon.jobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
